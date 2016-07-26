34aff5f16b3bbcf4cc2cf2831194fb9839353fba
boinfra <franck.boinay@gmail.com>
2016-07-26 13:30:00 +0200

    2nd use case working

--- SelectionTool/2nd use case/selection-tool.js
+++ SelectionTool/2nd use case/selection-tool.js
@@ -2,7 +2,6 @@ angular.module('noSubmitSelectionTool', ['noSubmitSelectionTool.services'])
 
     .controller('FilterController', ['$scope', 'ConfigService', 'DataService', 'SelectedDataService',
         function ($scope, ConfigService, DataService, SelectedDataService) {
-            $scope.title = "2nd use case";
             $scope.data = DataService.getData();
             $scope.useSubmitButton = ConfigService.useSubmitButton;
             $scope.jsonOutput = angular.toJson(SelectedDataService.selectedElements, 4);
